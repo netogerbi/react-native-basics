8e6d91ce02b0be86416c5e5d7649e37094085228
Grupo5-Reculos Gerbi Neto <netogerbi2@hotmail.com>
2019-07-31 21:32:09 -0300

    different types of functions declarations and respective functions calls

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,8 +8,23 @@ export default class Counter extends Component {
 
     }
 
-    oneMore = () => {this.setState({ number: this.state.num ++ });}
+    constructor(props) {
+        super(props);
 
+        // pode-se inicializar o estado no constutor
+        // this.state = {/* ... */}
+
+        // utilizando o bind no construtor pode-se chamar a função como abaixo 'oneMore(){}'
+        // desta forma se vincula o objeto do tipo Couter à função oneMore
+        // caso nao se utilize o bind, pode-se chamar a função da seguinte forma: 'onPress={() => this.oneMore()}'
+        this.oneMore = this.oneMore.bind(this); // 'this' representa uma instancia de contador
+    }
+
+    oneMore() { // criando a função tradicional, o this se refere a quem chamou a função, no caso o 'onPress', o que resulta em erro! para isso utiliza-se o bind acima
+        this.setState({ number: this.state.num ++ });
+    }
+
+    // usando arrow function, o escopo da função é a instancia atual
     clear = () => this.setState({ num: 0 });
 
     render() {
